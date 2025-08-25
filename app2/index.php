26c203746e30164c3b8fe656d8eabc2aee2f64aa
JoaoSouza217 <163845319+JoaoSouza217@users.noreply.github.com>
2025-08-25 08:42:57 -0300

    Create index.php

---
+++ app2/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulário de Contato</title>
+</head>
+<body>
+  <h1>Fale Conosco</h1>
+
+  <form method="post" action="">
+      <label for="nome">Nome:</label>
+      <input type="text" name="nome" id="nome" required><br><br>
+
+      <label for="email">Email:</label>
+      <input type="email" name="email" id="email" required><br><br>
+
+      <label for="mensagem">Mensagem:</label>
+      <textarea name="mensagem" id="mensagem" rows="5" required></textarea><br><br>
+
+      <input type="submit" value="Enviar">
+  </form>
+    
+ <?php
+ if($_SERVER["REQUEST_METHOD"] == "POST"){
+     //Coleta os dados do formulário
+
+     $nome = htmlspecialchars($_POST["nome"]);
+     $email = htmlspecialchars($_POST["email"]);
+     $mensagem = htmlspecialchars($_POST["mensagem"]);
+
+     echo ""
+ }
+?>       
+</body>
+</html>
