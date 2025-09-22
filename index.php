7e9f446b8ba927283fbee1ac9cce947b28bde873
JoaoSouza217 <163845319+JoaoSouza217@users.noreply.github.com>
2025-09-22 07:38:00 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
+<?php
 //Função para converter uma string para binário
 function stringParaBinario($string) {
-//A função str_split divide a string em um array de caracters
-//A função do ord pega o valor ASCII em binário
-$binario = '';
-foreach(str_split($string) as $char) {
-//Converte o caractere para o valor binário de 8 bits
-$binario .= str_pad(decbin(ord($char)), 8, '0', STR_PAD_LEFT) . ' ';
-}
-return $binario;
+  //A função str_split divide a string em um array de caracters
+  //A função do ord pega o valor ASCII em binário
+  $binario = '';
+  foreach(str_split($string) as $char) {
+    //Converte o caractere para o valor binário de 8 bits
+    $binario .= str_pad(decbin(ord($char)), 8, '0', STR_PAD_LEFT) . ' ';
+  }
+  return $binario;
 
 }
 
@@ -18,3 +19,4 @@ $fraseBinario = stringParaBinario($frase);
 //Exibindo a frase original e sua versão binária
 echo "Frase original: " . $frase . "<br>";
 echo "Frase em binário: " . $fraseParaBinario;
+?>
